c33c25e9f6dfbdba06dfc1a9d03996b450587dbf
manalalzeer <manalmaz01@gmail.com>
2021-10-25 16:43:46 +0300

    upload

---
+++ lab2/src/components/Footer.js
@@ -0,0 +1,16 @@
+function Footer() {
+
+
+  
+    return (
+      
+      <div className='footer'>
+      <button>Contact Us</button>
+      </div>
+  
+    );
+  }
+  
+  
+  export default Footer;
+  
---
+++ lab2/src/components/Header.js
@@ -0,0 +1,14 @@
+import header from "../Header.jpg";
+
+function Header() {  
+  return (
+    
+    <div>
+        <img src={header} width="100%" alt=""/>
+    </div>
+
+  );
+}
+
+
+export default Header;
