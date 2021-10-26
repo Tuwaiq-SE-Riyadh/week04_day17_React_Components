b18b805e24d3d6ca224e602b88e36ef85dd85f99
S-MW <72529306+S-MW@users.noreply.github.com>
2021-10-26 16:06:33 +0300

    add Header & Post

---
+++ components-of-react/src/components/Header.js
@@ -0,0 +1,14 @@
+import './Header.css';
+
+function Header() {
+    return (
+        <>
+        <div className="navBarClass">
+        </div>
+        <h1 className="WelcomeClass">Welcome To My Blog !</h1>
+
+        </>
+    );
+    }
+
+export default Header;
