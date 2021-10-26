9c0ba9224c37fcde3d339a7534b6c8d1f0dd7f6b
thuriabdullah <thiii302081@gmail.com>
2021-10-26 07:58:40 +0300

    lab

---
+++ my-laab/src/components/header.js
@@ -0,0 +1,16 @@
+import react from react
+function header(props)
+return(
+    <div className="header">
+        
+        <img src={props.img}/>
+        <div className="title">
+            <h2>{oprops.title}</h2>
+            </div>
+        
+ 
+
+
+    </div>
+)
+export default header;
